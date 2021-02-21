cd40b706cc7ae00c40febc00b6907596e14d2271
Ara Park <siyual.bak@gmail.com>
2021-02-21 16:35:35 +0900

    feat: refactoring (#36)

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 export { default as Injector } from "./injector";
 export { default as Extractor } from "./extractor";
 export { default as Position } from "./position";
+export { default as DefaultPosition } from "./default-position";
 
 export { default as context } from "./context";
 export { default as state } from "./state";
--- lib/state.ts
+++ lib/state.ts
@@ -1,14 +1,14 @@
 import { DefaultState } from "koa";
 import Position from "./position";
+import DefaultPosition from "./default-position";
 
 function state<StateT = DefaultState>(key: keyof StateT): Position<StateT> {
-  return {
+  return new DefaultPosition({
     inject: (ctx, value: StateT[keyof StateT]): void => {
-      if (value !== undefined) {
-        ctx.state[key] = value;
-      }
+      ctx.state[key] = value;
     },
     extract: (ctx) => ctx.state[key],
-  };
+  });
 }
+
 export default state;
