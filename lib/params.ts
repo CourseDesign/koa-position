01e9ebfffd46d94ad3da42b8abf6d5636a6341bf
siyual.park <siyual.bak@gmail.com>
2021-03-10 17:14:34 +0900

    fix: change bind context

--- lib/params.ts
+++ lib/params.ts
@@ -4,18 +4,18 @@ import DefaultPosition from "./default-position";
 
 function params<CustomT = Record<string, string>>(
   key?: keyof CustomT
-): Position<DefaultState, { request: { params: CustomT } }> {
+): Position<DefaultState, { params: CustomT }> {
   return new DefaultPosition({
     inject: (ctx, value): void => {
       if (key !== undefined) {
-        ctx.request.params[key] = value as never;
+        ctx.params[key] = value as never;
       } else {
-        ctx.request.params = value as CustomT;
+        ctx.params = value as CustomT;
       }
     },
     extract: (ctx) => {
       if (key !== undefined) {
-        return ctx.request.params[key];
+        return ctx.params[key];
       }
       return ctx;
     },
